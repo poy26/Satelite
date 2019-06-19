d1d47deff53093202be9e3054e6775c51fc7a540
poy26 <poycorpsdev@gmail.com>
2019-06-19 21:41:26 +0700

    Create Satelite.sh

---
+++ Satelite.sh
@@ -0,0 +1,244 @@
+#!/bin/bash
+#CodeByT_CSN~Tn.Yop
+clear
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo "---------------------------------------------------------" |lolcat
+toilet -f pagga "        Satelite      " -F gay |lolcat
+echo "---------------------------------------------------------" |lolcat
+echo $me'[+]─────────────────────────────────────────────────────[+]'
+echo $ij' |  •••••••••••  |[ Created By Tn.Yop ]|  •••••••••••••  |'
+echo $pu' | ─────────────────-─────────────────────────────────── |'
+echo $me' | VERSION Termux: 5.0.7                                 |'
+echo $me' | Name          : Tn.Yop                                |'
+echo $me' | CodeName      : T_CSN                                 |'
+echo $pu' | Instagram     : @tn.yop_engineering                   |'
+echo $pu' | Thanks To     : Allah SWT                             |'
+echo $pu' | COPYRIGHT     : T_CSN~Tn.Yop_Tech<InDo                |'
+echo $me' | ─────────────────-─────────────────────────────────── |'
+echo $ij' |  •••••••••••  |[ Created By Tn.Yop ]|  •••••••••••••  |'
+echo $pu'[+]─────────────────────────────────────────────────────[+]'
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $me"[+]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo $cy"[+]> Thanks"
+sleep 1
+echo ""
+echo $pu"[+]> see you gaes :)..."
+sleep 1
+exit
+}
+
+echo ""
+echo $me'-───────────────────────────────────────────'
+echo $ij'             Menu Hack Satelit'
+echo $pu'-───────────────────────────────────────────'
+echo $pr'«1»' $bi'Satelit PALAPA                          |'
+echo $pr'«2»' $ij'Satelit NASA                            |'
+echo $pr'«3»' $cy'Satelit TELKOM                          |'
+echo $pr'«4»' $me'Satelit GARUDA                          |'
+echo $pr'«5»' $pu'Satelit LAPAN                           |'
+echo $pr'«0»' $ku'EXIT MY PROGRAM                         |'
+echo $me'-───────────────────────────────────────────'
+echo $ij'             Menu Hack Satelit'
+echo $pu'-───────────────────────────────────────────'
+echo $bi'╭──'$ij'{'$pr'Pilih'$ku'⚔'$me'~'$ij'}'
+read -p '╰───────‹T_CSN~Tn.Yop›=»' peak;
+
+if [ $peak = 1 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau Arah Mana?"$ij"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau Geser Berapa Derajat?"$me"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" raja;
+sleep 1
+echo $cy'PROSES MEMINDAHKAN SATELITE SEDANG BERLANGSUNG ...'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'PROSES MEMINDAHKAN SATELITE SELESAI ...'
+sleep 1
+echo $bi'Satelit Palapa Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 2 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau KeArah Mana?"$ij"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau DiGeser Berapa Derajat?"$me"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" raja;
+sleep 1
+echo $cy'PROSES MEMINDAHKAN SATELITE SEDANG BERLANGSUNG ...'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'PROSES MEMINDAHKAN SATELITE SELESAI ...'
+sleep 1
+echo $bi'Satelit Nasa Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 3 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau KeArah Mana?"$ij"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau DiGeser Berapa Derajat?"$me"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" raja;
+sleep 1
+echo $cy'PROSES MEMINDAHKAN SATELITE SEDANG BERLANGSUNG ...'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'PROSES MEMINDAHKAN SATELITE SELESAI ...'
+sleep 1
+echo $bi'Satelit Telkom Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 4 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau KeArah Mana?"$ij"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau DiGeser Berapa Derajat?"$me"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" raja;
+sleep 1
+echo $cy'PROSES MEMINDAHKAN SATELITE SEDANG BERLANGSUNG ...'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'PROSES MEMINDAHKAN SATELITE SELESAI ...'
+sleep 1
+echo $bi'Satelit Garuda Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 5 ]
+then
+echo $bi"╭──"$ij"{"$pr"Mau KeArah Mana?"$ij"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" ara;
+sleep 1
+echo $pr"╭──"$me"{"$cy"Mau DiGeser Berapa Derajat?"$me"}"
+read -p"╰───────‹T_CSN~Tn.Yop›=»" raja;
+sleep 1
+echo $cy'PROSES MEMINDAHKAN SATELITE SEDANG BERLANGSUNG ...'
+sleep 1
+echo $me'10%'
+sleep 1
+echo $pu'20%'
+sleep 1
+echo $ku'30%'
+sleep 1
+echo $or'40%'
+sleep 1
+echo $bi'50%'
+sleep 1
+echo $ij'60%'
+sleep 1
+echo $cy'70%'
+sleep 1
+echo $me'80%'
+sleep 1
+echo $pu'90%'
+sleep 1
+echo $ku'100%'
+sleep 1
+echo $or'PROSES MEMINDAHKAN SATELITE SELESAI ...'
+sleep 1
+echo $bi'Satelit Lapan Berhasil DiGeser Ke Arah' $ara 'Sejauh' $raja 'Derajat'
+exit
+fi
+
+if [ $peak = 0 ]
+then
+echo 'wkwkwk tools ini cuma prank jangan baper' |lolcat
+echo 'FB : Poy' |lolcat
+exit
+fi
